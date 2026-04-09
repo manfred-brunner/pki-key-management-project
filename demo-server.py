44c2d69ed5a4dd588d184280b2acb5cde7a7b9bc
Manni <manni-b@t-online.de>
2026-04-09 16:52:04 +0200

    Initial PKI + Azure Key Vault project

---
+++ demo-server.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl
+
+server_address = ('localhost', 4443)
+httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+
+# Moderner SSL Context
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(
+    certfile="pki/issued/demo-api/server-fullchain.cert.pem",
+    keyfile="pki/issued/demo-api/server.key.pem"
+)
+
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+print("Server läuft auf https://localhost:4443")
+httpd.serve_forever()
